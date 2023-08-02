05f661877ccd9840df35ba0ba26a888195b35949
Air <air@sarzeez-2.local>
2023-08-02 13:55:51 +0500

    setting tailwind css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-component-interaction';
+  isDarkMode: boolean = false;
+
+  changeTheme() {
+    if (this.isDarkMode) {
+      this.isDarkMode = false;
+      return;
+    }
+    this.isDarkMode = true;
+  }
 }
